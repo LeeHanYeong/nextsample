d3e3ff862e3c4b3957914811c52512b621c19db5
lhy <dev@lhy.kr>
2021-06-23 12:35:17 +0900

    환경변수 브라우저 출력

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+console.log('index process.env:', process.env);
+
 function HomePage() {
     console.log('HomePage process.env:', process.env);
+    console.log('PUBLIC ABCD:', process.env.NEXT_PUBLIC_ABCD);
+    console.log('NORMAL ABCD:', process.env.NORMAL_ABCD);
     return <div>Welcome to Next.js!</div>
 }
 
